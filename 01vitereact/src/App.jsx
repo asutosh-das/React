315409a5b0eb75bd65ce6e9568656dd0a6d87c8a
asutosh-das <146555096+asutosh-das@users.noreply.github.com>
2025-07-17 20:37:17 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Chai from "./chai"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    <h1>Chai Aur React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, autem?</p>
+    </>
+    
+  )
+}
+
+export default App
